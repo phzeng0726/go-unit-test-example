9a9feecf4fc26fbfb3909789a83b232947b9ddc2
Rita Zeng <A004005@SERCOMM.COM>
2024-06-27 10:33:10 +0800

    perf(test): improve simple test with testify package

--- server/note/std_test.go
+++ server/note/std_test.go
@@ -9,8 +9,8 @@ import (
 // Testing有分T(Test)跟B(Benchmark)，前者測Bug，後者測速度
 func TestIsPositive(t *testing.T) {
 	// 測試負數
-	assert.False(t, IsPositive(-1), "IsPositive(-1) should return false")
+	assert.Equal(t, false, IsPositive(-1), "IsPositive(-1) should return false")
 
 	// 測試正數
-	assert.True(t, IsPositive(1), "IsPositive(1) should return true")
+	assert.Equal(t, true, IsPositive(1), "IsPositive(1) should return true")
 }
